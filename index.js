54f80305a2e27d0907d1f0cadfad9e168b821757
dpashok <ashokdpch@gmail.com>
2023-06-11 12:56:54 +0000

    initail commit

---
+++ index.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+    jwt.verify(jwtToken, "secretKey", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid Access Token");
+      } else {
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
+
+///get Profile Api
+app.get("/profile/", authenticateToken, async (request, response) => {
+  const { username } = request;
+  const getProfileQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(getProfileQuery);
+  response.send(dbUser);
+});
+
+// Get Books API
+app.get("/books/", authenticateToken, async (request, response) => {
+  const getBooksQuery = `
+                    SELECT
+                        *
+                    FROM
+                        book
+                    ORDER BY
+                        book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
+
+// User Register API
+app.post("/users/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const selectUserQuery = `
+    SELECT 
+      * 
+    FROM 
+      user 
+    WHERE 
+      username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+     INSERT INTO
+      user (username, name, password, gender, location)
+     VALUES
+      (
+       '${username}',
+       '${name}',
+       '${hashedPassword}',
+       '${gender}',
+       '${location}'  
+      );`;
+    await db.run(createUserQuery);
+    response.send("User created successfully");
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+// User Login API
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+
+  const selectUserQuery = `
+    SELECT
+      *
+    FROM
+      user
+    WHERE 
+      username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = { username: username };
+      const jwtToken = jwt.sign(payload, "secretKey");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
